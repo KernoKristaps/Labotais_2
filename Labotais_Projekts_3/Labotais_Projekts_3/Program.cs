7176bd26f48fca3f4014dfe70e19bbbad1ba8883
KernoKristaps <39062321+KernoKristaps@users.noreply.github.com>
2018-05-07 20:40:20 +0300

    Darbs_2
    Izlabots

---
+++ Labotais_Projekts_3/Labotais_Projekts_3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labotais_Projekts_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String dati;
+
+            Console.WriteLine("Kad Tu esi dzimis ?");
+
+            dati = Console.ReadLine();
+            Console.WriteLine("Dzimis" + dati);
+            Console.ReadLine();
+            Console.WriteLine("github plus");
+            Console.ReadLine();
+        }
+    }
+}
